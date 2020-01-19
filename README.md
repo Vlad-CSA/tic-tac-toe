a689fd7b68b21781f0e6c108aaddad1e1e799b0a
Piratt14 <satdvlad@gmail.com>
2020-01-19 08:29:40 +0300

    первая полностью работающая версия со всем функционалом

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+#Tic Tac Toe
+
 ## Available Scripts
 
 In the project directory, you can run:
@@ -12,57 +14,17 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
-### `npm test`
+----
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+Краткое описание:
+- Для запуска надо скачать npm пакеты, для этого в консоли набрать npm install
+- Для доступа к игре надо ввести верное имя пользоветеля и пароль, иначе будет выведено сообщение о неверных данных
+- Если попытаться вручную перейти на нужный раздел приложения, без авторизации, то произойдет редирект на страницу входа
+- Пользователь ходим "крестиком" и ходит первым, затем следует нажать кнопку "Computer turn" для того, что бы компьютер сделал случайный ход
+- В любой момент времени можно переходить по истории ходов и менять ходы свои и компьютера(В рамках текущей партии)
+- После победы одного из участников, или ничьей - надо нажать на кнопку "Next Match"
+- Для выхода из игры(деавторизации) надо нажать на кнопку "Exit"
+- В любой момент времени данные синхронизируются с сервером, поэтому можно продолжить незаконченную партию в любой момент после повторного входа.
+Не важно как был осуществлен выход, по кнопке или закрыв страницу или браузер.
+- Статистика матчей тоже сохраняется на сервере
+- Под тремя кнопками "Computer Turn", "Next Match", "Exit" - показано кто ходит следующий, %username% или компьютер, так же там отображается в конце раунда кто победил или произошла ничья
--- src/index.js
+++ src/index.js
@@ -1,12 +1,425 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
+    return (
+        <button
+            className="square"
+            onClick={ props.onClick }>
+            { props.value }
+        </button>
+    );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Board extends React.Component {
+
+    renderSquare(i) {
+        return (
+            <Square
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <div className={this.props.boardClasses}>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+            user: 'Vlad',
+            boardClasses: 'board',
+            statistics: {
+                user: {
+                    wins: 0,
+                    loses: 0,
+                    draws: 0,
+                },
+                computer: {
+                    wins: 0,
+                    loses: 0,
+                    draws: 0,
+                },
+            },
+        };
+        this.networkService = new NetworkService();
+        this.networkService
+            .receiveDataFromServer()
+            .then(res => res ? this.setState(res) : null);
+    }
+
+    handleClick(i) {
+        if (!this.state.xIsNext) return;
+
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length-1];
+        const squares = current.squares.slice();
+        //  если кто то выиграл или поле заполнено, то выходим из функции
+        if (calculateWinner(squares).winner || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares,
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+            boardClasses: 'board',
+        })
+    }
+
+    randomTurn() {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length-1];
+        const squares = current.squares.slice();
+
+        //  если ход игрока то компьютеру запрещено ходить
+        if (this.state.xIsNext || calculateWinner(squares).winner) return;
+
+        while(true) {
+            const i = Math.floor(Math.random() * 9);
+            if (squares[i]) continue;
+            squares[i] = 'O';
+            this.setState({
+                history: history.concat([{
+                    squares,
+                }]),
+                stepNumber: history.length,
+                xIsNext: !this.state.xIsNext,
+            });
+            return;
+        }
+    }
+
+    handleNextMatch(winner) {
+        if (!winner) return;
+        this.setState({
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+            boardClasses: 'board',
+            statistics: {
+                user: {
+                    wins: winner === 'X' ? this.state.statistics.user.wins + 1 : this.state.statistics.user.wins,
+                    loses: winner === 'O' ? this.state.statistics.user.loses + 1 : this.state.statistics.user.loses,
+                    draws: winner === 'nobody' ? this.state.statistics.user.draws + 1 : this.state.statistics.user.draws,
+                },
+                computer: {
+                    wins: winner === 'O' ? this.state.statistics.computer.wins + 1 : this.state.statistics.computer.wins,
+                    loses: winner === 'X' ? this.state.statistics.computer.loses + 1 : this.state.statistics.computer.loses,
+                    draws: winner === 'nobody' ? this.state.statistics.computer.draws + 1 : this.state.statistics.computer.draws,
+                },
+            }
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        //  отправляем весь state на сервер, потому что выбора особо нет
+        //  в данном случае нам дается возможность управлять только одной
+        //  строкой с данными на сервере
+        this.networkService
+            .sendDataFromUser(this.state);
+    }
+
+    render() {
+
+        const { history } = this.state;
+        const current = history[this.state.stepNumber];
+        //  небольшой прием, что бы не использовать переменную с глобальной областью видимости как подумал изначально
+        const { winner, i } = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                `Go to move #${move}` :
+                `Go to game start`;
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        let status;
+        if (winner) {
+            if (winner === 'nobody') {
+                status = 'Winner: nobody';
+            } else {
+                // !!! переделать так что бы не изменять стейт из под рендера
+                if (!this.state.boardClasses.includes(`board-${i}`)) {
+                    this.setState({
+                        boardClasses: `${this.state.boardClasses} board-${i}`
+                    });
+                }
+                status = `Winner: ${winner === 'X' ? this.state.user + '(X)' : 'Computer(O)'}`;
+            }
+        } else {
+            status = `Next player: ${this.state.xIsNext ? this.state.user + '(X)' : 'computer(O)'}`;
+        }
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                        // тут "костыль" с классами, что бы реализовать перечеркивание
+                        boardClasses={this.state.boardClasses}
+                    />
+                </div>
+                <div className="game-info">
+                    <button onClick={() => this.randomTurn()}>Computer Turn</button><br/>
+                    <button
+                        onClick={() => this.handleNextMatch(winner)}
+                        style={{color: "blue"}}>
+                        Next Match
+                    </button>
+                    <button onClick={this.props.onExit}>Exit</button><br/>
+                    <div>{ status }</div>
+                    <ol>{ moves }</ol>
+                </div>
+                <div className="statistics">
+                    <div>
+                        <p><b>{this.state.user}</b></p>
+                        <ul>
+                            <li>Wins: <b>{this.state.statistics.user.wins}</b></li>
+                            <li>Loses: <b>{this.state.statistics.user.loses}</b></li>
+                            <li>Draws: <b>{this.state.statistics.user.draws}</b></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p><b>Computer</b></p>
+                        <ul>
+                            <li>Wins: <b>{this.state.statistics.computer.wins}</b></li>
+                            <li>Loses: <b>{this.state.statistics.computer.loses}</b></li>
+                            <li>Draws: <b>{this.state.statistics.computer.draws}</b></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+const LoginPage = ({ isLoggedIn, onLogin, hasError }) => {
+
+    if (isLoggedIn) {
+        return <Redirect to="/game" />
+    }
+
+    const Error = hasError ? <h3 style={{color: "red"}}>Wrong username or password</h3> : null;
+
+    return (
+        <div className="login-form">
+            <h2>Welcome to Tic Tac Toe</h2>
+            <p>Login to play the game</p>
+            <p>
+                <label htmlFor="user">User name</label>
+                <input type="text" id="user" name="user"/>
+            </p>
+            <p>
+                <label htmlFor="pass">Password</label>
+                <input type="text" id="pass" name="pass"/>
+            </p>
+            <button
+                onClick={onLogin}>
+                Enter
+            </button>
+            {Error}
+        </div>
+    );
+};
+
+const SecretPage = ({ isLoggedIn, onExit }) => {
+
+    if (isLoggedIn) {
+        return (
+            <Game onExit={onExit}/>
+        );
+    }
+
+    return <Redirect to="/login" />;
+};
+
+class NetworkService {
+
+    // реализовал все методы для работы с API, хоть по сути можно обойтись 4-мя
+
+    _apiBase = 'http://localhost:8421/api.';
+
+    signInReq = async (data) => {
+        return await fetch(`${this._apiBase}authentication.signin`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            // нужно для передачи cookie
+            credentials: 'include',
+            body: JSON.stringify(data),
+        });
+    };
+
+    signOutReq = async () => {
+        return await fetch(`${this._apiBase}authentication.signout`, {
+            method: 'GET',
+            credentials: 'include',
+        });
+    };
+
+    userCheck = async () => {
+        return await fetch(`${this._apiBase}authentication.check`,{
+            method: 'GET',
+            credentials: 'include',
+        });
+    };
+
+    sendDataFromUser = async (data) =>{
+        return await fetch(`${this._apiBase}user.setstate`, {
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify(data),
+        });
+    };
+
+    receiveDataFromServer = async () => {
+        return await fetch(`${this._apiBase}user.getstate`, {
+            method: 'GET',
+            credentials: 'include',
+        }).then(res => res.json())
+    }
+}
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false,
+            hasError: false,
+            networkService: new NetworkService(),
+        };
+    }
+
+    onLogin = () => {
+        const userInfo = {
+            "username": document.getElementById('user').value,
+            "password": document.getElementById('pass').value
+        };
+        this.state.networkService
+            .signInReq(userInfo)
+            .then(res => {
+                if (res.ok) {
+                    this.setState({ isLoggedIn: true });
+                } else {
+                    this.setState({ hasError: true });
+                }
+            });
+    };
+
+    onExit = () => {
+        // sign out request
+        this.state.networkService
+            .signOutReq()
+            .then(res => {
+                if (res.ok) {
+                    this.setState({ isLoggedIn: false });
+                }
+            });
+    };
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/login"
+                           render={() =>
+                               <LoginPage
+                                   isLoggedIn={this.state.isLoggedIn}
+                                   onLogin={this.onLogin}
+                                   hasError={this.state.hasError}/>
+                           }/>
+                    <Route path="/game"
+                           render={() =>
+                               <SecretPage
+                                   isLoggedIn={this.state.isLoggedIn}
+                                   onExit={this.onExit}/>
+                           }/>
+                    <Redirect to="/login"/>
+                    {/* альтернативный вариант неправильных адресов */}
+                    {/* но redirect более user-friendly как по мне, особенно для SPA */}
+                    <Route render={() => <h2>Page not found</h2>} />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            console.log({ winner:squares[a], i });
+            return { winner:squares[a], i };
+        }
+    }
+    //  проверка на ничью
+    if (squares.filter((i)=>i).length === 9) return { winner:'nobody', i:null };
+    return { winner: null, i: null };
+}
